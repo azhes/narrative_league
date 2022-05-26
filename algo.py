996270b76824b97fbf98a37d8b0fd657e80b9ea9
Andy <77294025+azhes@users.noreply.github.com>
2022-05-26 16:20:48 -0700

    Add files via upload

--- algo.py
+++ algo.py
@@ -1,6 +1,12 @@
 import math
 import xlrd
 
+class Bracket:
+    def __init__(self, divide, bottom, num_brackets=5):
+        self.divide = divide
+        self.bottom = bottom + 1
+        self.top = bottom + 1 + divide
+
 def get_data():
     loc = ("test_scores.xls")
     wb = xlrd.open_workbook(loc)
@@ -13,45 +19,27 @@ def get_data():
 
     return num_players, player_dict
 
-def brackets(scores_total, num_brackets=5):
+def create_brackets(scores_total, num_brackets=5):
     divide = math.ceil(scores_total / num_brackets)
-    bracket1 = {
-        "bottom": 0,
-        "top": 0 + divide
-    }
+    bracket1 = Bracket(divide, -1)
+    bracket2 = Bracket(divide, bracket1.top + 1)
+    bracket3 = Bracket(divide, bracket2.top + 1)
+    bracket4 = Bracket(divide, bracket3.top + 1)
+    bracket5 = Bracket(divide, bracket4.top + 1)
 
-    bracket2 = {
-        "bottom": bracket1["top"] + 1,
-        "top": bracket1["top"] + 1 + divide
-    }
+    brackets = [bracket1, bracket2, bracket3, bracket4, bracket5]
 
-    bracket3 = {
-        "bottom": bracket2["top"] + 1,
-        "top": bracket2["top"] + 1 + divide
-    }
+    return brackets
 
-    bracket4 = {
-        "bottom": bracket3["top"] + 1,
-        "top": bracket3["top"] + 1 + divide
-    }
-
-    bracket5 = {
-        "bottom": bracket4["top"] + 1,
-        "top": bracket4["top"] + 1 + divide
-
-    }
-
-    print(f'Bracket 1: {bracket1["bottom"]} - {bracket1["top"]}')
-    print(f'Bracket 2: {bracket2["bottom"]} - {bracket2["top"]}')
-    print(f'Bracket 3: {bracket3["bottom"]} - {bracket3["top"]}')
-    print(f'Bracket 4: {bracket4["bottom"]} - {bracket4["top"]}')
-    print(f'Bracket 5: {bracket5["bottom"]} - {bracket5["top"]}')
+# def populate_brackets(player_dict):
 
 def main(): 
     num_players, player_dict = get_data()
 
     scores_total = sum(player_dict.values())
 
-    brackets(scores_total)
+    brackets = create_brackets(scores_total)
+
+    print(brackets[3].bottom)
 
 main()
