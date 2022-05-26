02e564a0e42b2ae922ea1a3520ed335a66d1cc2b
Andy <77294025+azhes@users.noreply.github.com>
2022-05-26 16:58:34 -0700

    Add files via upload

--- algo.py
+++ algo.py
@@ -1,11 +1,18 @@
 import math
+from tkinter.font import names
 import xlrd
 
 class Bracket:
-    def __init__(self, divide, bottom, num_brackets=5):
+    def __init__(self, divide, bottom):
         self.divide = divide
-        self.bottom = bottom + 1
+        self.bottom = bottom
         self.top = bottom + 1 + divide
+
+class Player:
+    def __init__(self, name, score):
+        self.name = name
+        self.score = score
+        self.bracket = None
 
 def get_data():
     loc = ("test_scores.xls")
@@ -19,9 +26,9 @@ def get_data():
 
     return num_players, player_dict
 
-def create_brackets(scores_total, num_brackets=5):
-    divide = math.ceil(scores_total / num_brackets)
-    bracket1 = Bracket(divide, -1)
+def create_brackets(highest_score, num_brackets=5):
+    divide = math.ceil(highest_score / num_brackets)
+    bracket1 = Bracket(divide, 0)
     bracket2 = Bracket(divide, bracket1.top + 1)
     bracket3 = Bracket(divide, bracket2.top + 1)
     bracket4 = Bracket(divide, bracket3.top + 1)
@@ -31,15 +38,37 @@ def create_brackets(scores_total, num_brackets=5):
 
     return brackets
 
-# def populate_brackets(player_dict):
+def create_players(player_dict):
+    players_list = []
+    for player in player_dict:
+        player = Player(player, player_dict[player])
+        players_list.append(player)
+
+    return players_list
+
+def populate_brackets(players_list, brackets):
+    for player in players_list:
+        for bracket in brackets:
+            if bracket.bottom < player.score < bracket.top:
+                player.bracket = bracket
 
 def main(): 
     num_players, player_dict = get_data()
 
-    scores_total = sum(player_dict.values())
+    highest_score = max(player_dict.values())
 
-    brackets = create_brackets(scores_total)
+    brackets = create_brackets(highest_score)
+    print('Brackets:')
+    for bracket in brackets:
+        print(f'{bracket.bottom} - {bracket.top}')
 
-    print(brackets[3].bottom)
+    players_list = create_players(player_dict)
+
+    populate_brackets(players_list, brackets)
+    
+    print('Players:')
+    for player in players_list:
+        print(f'{player.name}: {player.score}\n Bracket: {player.bracket.bottom} - {player.bracket.top}\n')
+    
 
 main()
